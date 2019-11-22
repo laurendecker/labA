477fdcaed96a72614079cc7ca3ff8f351eb01c10
laurendecker <laurendecker@mymail.mines.edu>
2019-11-21 23:04:26 -0700

    FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -17,3 +17,11 @@ def UpdateString(str1, str2, x):
     str1 = ''.join(str1)
     updatedstring = str(str1)
     print('OUTPUT', updatedstring)
+def FindWordCount(list1, string1):
+    i = 0
+    list1 = (''.join(list1)).split()
+    for x in list1:
+        if x == string1:
+            i += 1
+    return i
+
